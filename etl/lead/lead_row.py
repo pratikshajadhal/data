898c9317d24761a6c1a720a5cb0c7a0a1632bb67
mertse7en <mert@truve.io>
2022-06-10 16:20:04 +0300

    Add lead bases

---
+++ etl/lead/lead_row.py
@@ -0,0 +1,24 @@
+from leaddocket.client import LeadDocketClient
+from etl.datamodel import LeadDocketConfig
+from etl.datamodel import ColumnConfig
+from etl.datamodel import ColumnDefn
+from etl.destination import ETLDestination, S3Destination
+import pandas as pd
+from .lead_modeletl import LeadModelETL
+
+
+class LeadRowETL(LeadModelETL):
+
+    def extract_data_from_source(self, statuses:list):
+        # Using this statuses get all lead row table.
+        return self.ld_client.get_lead_row(statuses)
+
+    def extract_lead_metadata(self):
+        # Get all status
+        return self.ld_client.get_statuses()
+
+    def transform(self, lead:dict):
+        return pd.DataFrame([lead])
+
+
+
