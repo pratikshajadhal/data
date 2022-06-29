5dccd50cbc44e0d86e738351baf54e190c210bd1
mertse7en <mert@truve.io>
2022-06-29 13:45:19 +0300

    Merge s3 functions

--- tasks/helper.py
+++ tasks/helper.py
@@ -6,23 +6,14 @@ from utils import get_logger
 logger = get_logger(__name__)
 
 
-def auth_s3():
-    # Session
-    # session = boto3.Session(
-    #          aws_access_key_id=os.environ["aws_access_key_id"],
-    #          aws_secret_access_key=os.environ["aws_secret_access_key"],)
-    # s3 = session.resource('s3')
-    # bucket = s3.Bucket('xxx') 
-
-    # client
-    return boto3.client(
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+def download_s3_file(s3_path: str, download_path: str):
+    s3_client = boto3.client(
         's3',
         aws_access_key_id = os.environ["aws_access_key_id"],
         aws_secret_access_key = os.environ["aws_secret_access_key"]
     )
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-def download_s3_file(s3_client: boto3.client, s3_path: str, download_path: str):
     # Split s3 path: s3://dev-data-api-01-buckets-buckettruverawdata-8d0qeyh8pnrf/confs/filevine/config_6586.yaml
     bucket_name, key_name = split_s3_bucket_key(s3_path=s3_path)
 
@@ -36,25 +27,14 @@ def download_s3_file(s3_client: boto3.client, s3_path: str, download_path: str):
             raise
 
 
-def find_bucket_key(s3_path:str):
-    """
-    This is a *download_s3_file* helper function that given an s3 path such that the path is of
-    the form: bucket/key
-    It will return the bucket and the key represented by the s3 path
-    """
+def split_s3_bucket_key(s3_path:str):
+    if s3_path.startswith('s3://'):
+        s3_path = s3_path[5:]
+    
     s3_components = s3_path.split('/')
     bucket = s3_components[0]
     s3_key = ""
     if len(s3_components) > 1:
         s3_key = '/'.join(s3_components[1:])
     return bucket, s3_key
-
-def split_s3_bucket_key(s3_path:str):
-    """Split s3 path into bucket and key prefix.
-    This will also handle the s3:// prefix.
-    :return: Tuple of ('bucketname', 'keyname')
-    """
-    if s3_path.startswith('s3://'):
-        s3_path = s3_path[5:]
-    return find_bucket_key(s3_path)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
