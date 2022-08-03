c111585fcb71bb6afacf12e58e25a60fabb42403
Tyler <tyler@truve.io>
2022-08-03 19:55:10 +0000

    Merged in infra/split-changelog-steps (pull request #32)
    Infra: Split Changelog Steps
    
    - split changelog steps
    
    - write changelog before docker build/push
    
    - write/push changelog to branch after docker build/push
    
    - update pipeline
    
    - bump patch version

--- .infra/devops/deploy/bitbucket/changelog/push.sh
+++ .infra/devops/deploy/bitbucket/changelog/push.sh
@@ -6,19 +6,6 @@ set -e
 
 API_VERSION=$(cat version)
 
-# Path to Changelog Markdown template
-CHANGELOG_TEMPLATE_PATH=".infra/devops/deploy/bitbucket/changelog/template.md"
-
-# ChangeLog data content will be passed to nodejs using temp file
-CHANGELOG_CONTENT_TEMP_PATH=".infra/devops/deploy/bitbucket/changelog/rendered.temp"
-
-echo 
-echo "Render changelog from Git log..."
-git show $BITBUCKET_COMMIT --pretty=format:"Original Commit ID: %h%n%n## Changes%n%B" --no-patch > $CHANGELOG_CONTENT_TEMP_PATH
-
-# Write rendered changelog
-node .infra/devops/deploy/bitbucket/changelog/write.js $CHANGELOG_TEMPLATE_PATH $CHANGELOG_CONTENT_TEMP_PATH
-
 echo
 echo "Committing Changelog to current branch..."
 
