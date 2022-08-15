56b5a135575f36fd6f0280bbd8bb80421e86a0f2
mertse7en <mert@truve.io>
2022-08-15 12:42:26 +0300

    Change etlstatus and pipeline status pydantic model

--- api_server/config.py
+++ api_server/config.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from typing import Any, Dict, Literal
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator
+from enum import Enum
+from typing import Literal, List, Optional
+
 
 EVENT_TYPES = ["PhaseChanged", "Created", "Updated", "Deleted"]
 ENTITY_TYPES = ["Project", "Form", "CollectionItem"]
@@ -42,10 +45,10 @@ class Onboarding(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "org_id": "Foo",
-                "tpa_id": "FILEVINE",
+                "org_id": 123,
+                "tpa_id": "INSTAGRAM",
                 "credentials":{
-                    "api_key": "fvpk_f722dca1-73bb-9095-79fe-0a3069636a3f",
+                    "api_key": "foo",
                     "user_id": 31958
                     }
                 }
@@ -59,7 +62,7 @@ class Notify(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "org_id": "Foo",
+                "org_id": 123123,
                 "tpa_id": "FILEVINE",
                 }
             }
@@ -69,7 +72,7 @@ class TaskStatus(BaseModel):
     truve_id: int 
     tpa_id : str 
     job_result: dict
-    error_body: dict = None
+    error_body: Optional[dict]
 
     class Config:
         schema_extra = {
@@ -81,32 +84,34 @@ class TaskStatus(BaseModel):
                     }
                 }
             }
+
+# - - - - Update job complete
+class pipeline_statusses(str, Enum):
+    success='SUCCESS'
+    fail='FAILED'
 
 class EtlStatus(BaseModel):
     truve_id: int 
     tpa_id : str 
-    job_result: dict
-    error_body: dict = None
+    pipeline_status: pipeline_statuses
+    fail_detail: Optional[dict]
+
+    # #TODO: If pipeline status FAILED then fail detail needs to be required. Check how to solve
+    # TODO: Might dive detail. But if pipeline_Status fail then fail_detail must be required?
+    # @root_validator
+    # def validate_date(cls, values):
+    #     if values["pipeline_status"] == 'FAILED':
+    #         values["some_date"] = values["some_list"][0]
+    #     return values
+
 
     class Config:
+        use_enum_values = True  # <--
         schema_extra = {
             "example": {
-                    # TODO:
-                    "truve_id": 123,
-                    "tpa_id": "FILEVINE",
-                    "job_result":{
-                        "status": "success"
+                        "truve_id": 123,
+                        "tpa_id": "FILEVINE",
+                        "pipeline_status": "SUCCESS"
                     }
-                }
             }
-
-
-# @dataclass
-# class EtlStatus:
-#     truve_id: int 
-#     tpa_id : str 
-#     pipeline_status: pipeline_statuses
-#     fail_data: FailObject = None
-
-# - - -
-
+# - - - - 
