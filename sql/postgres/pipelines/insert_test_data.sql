05e5412d072b8d26ad72115f542dd2d108d8e27a
mertse7en <mert@truve.io>
2022-09-05 18:43:38 +0300

    Add dummy error

--- sql/postgres/pipelines/insert_test_data.sql
+++ sql/postgres/pipelines/insert_test_data.sql
@@ -4,9 +4,9 @@ INSERT INTO pipelines(uuid, org_uuid, tpa_identifier, pipeline_number, config_ya
     ('132a13a2-e30a-4596-a910-2e918378573f', '97b23e8a-ab86-4559-b686-09dc409f24b8', 'filevine', 2, '', 5, '2022-08-30 13:23:45', '2022-08-30 19:24:45', '2022-08-30 13:23:15', '2022-08-30 19:24:45')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO jobs(uuid, pipeline_id, job_identifier, status_id, started_at, ended_at, created_at, updated_at) VALUES
-    ('f3295e88-c206-4102-b891-aac979869b3d', 1, 'INSTAGRAM_PULL_POSTS', 3, '2022-08-30 11:23:45', '2022-08-30 11:24:45', '2022-08-30 11:23:15', '2022-08-30 11:24:45')
-    ('6eef4911-1060-420b-b302-9ea8827a992f', 1, 'INSTAGRAM_PULL_AD_ACTIVITY', 3, '2022-08-30 11:23:45', '2022-08-30 11:24:45', '2022-08-30 11:23:15', '2022-08-30 11:24:45')
-    ('d30e91c5-1c2c-4458-b9b0-e15bca96fe98', 2, 'FILEVINE_HISTORICAL_PULL', 3, '2022-08-30 12:23:45', '2022-08-30 18:23:45', '2022-08-30 12:23:15', '2022-08-30 18:23:45')
-    ('c520d1e2-f0d4-4ee3-99bd-3777d0f9bb79', 3, 'FILEVINE_HISTORICAL_PULL', 3, '2022-08-30 13:23:45', '2022-08-30 19:24:45', '2022-08-30 13:23:15', '2022-08-30 19:23:45')
-ON CONFLICT DO NOTHING;
+INSERT INTO jobs(uuid, pipeline_id, job_identifier, status_id, error_reason_id, error_details, started_at, ended_at, created_at, updated_at) VALUES
+    ('f3295e88-c206-4102-b891-aac979869b3d', 1, 'INSTAGRAM_PULL_POSTS', 3, 2, '{ "error_detail": "ABC"}', '2022-08-30 11:23:45', '2022-08-30 11:24:45', '2022-08-30 11:23:15', '2022-08-30 11:24:45'),
+    ('6eef4911-1060-420b-b302-9ea8827a992f', 1, 'INSTAGRAM_PULL_AD_ACTIVITY', 3, 1, '{ "error_detail": "ABC"}',  '2022-08-30 11:23:45', '2022-08-30 11:24:45', '2022-08-30 11:23:15', '2022-08-30 11:24:45'),
+    ('d30e91c5-1c2c-4458-b9b0-e15bca96fe98', 2, 'FILEVINE_HISTORICAL_PULL', 3, 1, '{ "error_detail": "ABC"}',  '2022-08-30 12:23:45', '2022-08-30 18:23:45', '2022-08-30 12:23:15', '2022-08-30 18:23:45'),
+    ('c520d1e2-f0d4-4ee3-99bd-3777d0f9bb79', 3, 'FILEVINE_HISTORICAL_PULL', 3, 3, '{ "error_detail": "ABC"}',  '2022-08-30 13:23:45', '2022-08-30 19:24:45', '2022-08-30 13:23:15', '2022-08-30 19:23:45')
+ON CONFLICT DO nothing;
