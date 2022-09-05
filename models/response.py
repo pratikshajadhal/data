fc0a0482f29f57ed8770595b536cdc72294a7225
mertse7en <mert@truve.io>
2022-09-05 18:43:21 +0300

    Add response dataclass

--- models/response.py
+++ models/response.py
@@ -6,12 +6,13 @@ from pydantic import BaseModel
 
 from models.request import ExecStatus, ErrorReason
 
-
-
-
 #TODO:
 class Status(BaseModel):
     latest_pipeline_number: int
     latest_pipeline_status: ExecStatus
     reason: Optional[ErrorReason]
     details: Optional[str]
+
+class ErrorData(BaseModel):
+    error_reason_id: int or None
+    error_reason_status: dict or None
